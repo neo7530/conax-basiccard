db1ad2b022c481d8430ebe89d15eb24ee73f1612
Marco Wabbel <mw@alogis.com>
2020-12-01 14:10:47 +0100

    update to caid 0x0B02

--- Key-Hacker/main.c
+++ Key-Hacker/main.c
@@ -27,7 +27,8 @@ compile with the command: gcc demo_rx.c rs232.c -Wall -Wextra -o2 -o test_rx
 unsigned char check;
 int   cport_nr, bdrate,serialno,groupid;
 uint8_t parsebuf[1024];
-uint8_t systemkey[16] = {0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x20,0x21,0x22,0x23,0x24,0x25};
+unsigned char updatebuffer[1024] = {0};
+uint8_t systemkey[16]={0};// = {0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x20,0x21,0x22,0x23,0x24,0x25};
 
 void pen(int duration){
 #ifdef _WIN32
@@ -89,70 +90,42 @@ void parse(uint8_t l){
 
 
 
-uint16_t getdata(void){
-    uint16_t n,i;
-    unsigned char buf[4096];
-while(n < 1){
-    n = RS232_PollComport(cport_nr, buf, 4095);
-    if(n > 0)
-    {
-      //printf("%d\n\n",n);
-      buf[n] = 0;   /* always put a "null" at the end of a string! */
-
-      for(i=0; i < n; i++)
-      {
-        printf("%02X ",buf[i]);
-        parsebuf[i] = buf[i];
-        if(i%16==15){
-            printf("\n");
-        }
-      }
-    }
-}
-    //if(n>2){
-        i = buf[n-2];
-        i <<= 8;
-    //}
-    i |= buf[n-1];
-    return i;
-}
-
 void command(char *cmd,char *data, int pause){
-    uint16_t l,r,x;
+    uint16_t l=cmd[4],x,r;
+    unsigned char buf[4096] = {0};
     char rsponse[5] = {0xdd,0xca,0x00,0x00,0x00};
+
     RS232_SendBuf(cport_nr,cmd,5);
     printf("=> ");
-    l = getdata() & 0xff;
-    printf("\n<= ");
-    r = getdata() & 0xff;
-    //if(r == cmd[1]){
-        RS232_SendBuf(cport_nr,data,l);
-        printf("\n=> ");
-        getdata();
-    //}
+    pen(20);
+    RS232_SendBuf(cport_nr,data,l);
     pen(pause);
-    printf("\n<= ");
-    l = x = getdata();
-    printf("\n");
-do{
-    if(l > 0x9800){
-        printf("\n");
-        rsponse[4] = l & 255;
-        RS232_SendBuf(cport_nr,rsponse,5);
-        printf("=> ");
-        getdata();
-        printf("\n<= ");
-        r =  getdata() & 255;
-        if(r == 0xCA){
-            pen(100);
-            printf("\n<=\n");
-            l = getdata();
-            printf("\n");
-        }
-        parse(x);
+    for(int i = 0;i<6+l+2;i++){
+        RS232_PollComport(cport_nr, buf, 4095);
+        printf("%02x ",buf[i]);
+        r |= buf[i-1];
+        r <<= 8;
+        r |= buf[i];
     }
+do{
+    if(r > 0x9800){
+        printf("\n");
+        rsponse[4] = l = r & 255;
+        RS232_SendBuf(cport_nr,rsponse,5);
+        printf("<= ");
+        pen(pause);
+        for(int i = 0;i<6+l+2;i++){
+            RS232_PollComport(cport_nr, buf, 4095);
+            printf("%02x ",buf[i]);
+            parsebuf[i] = buf[i];
+            r |= buf[i-1];
+            r <<= 8;
+            r |= buf[i];
+        }
+        parse(6+l+2);
+        }
     printf("\n");
-}while(l != 0x9000 && l != 0x9011 && l != 0x9017);
+}while(r != 0x9000 && r != 0x9011 && r != 0x9017);
 }
 
 void init(void){
@@ -173,7 +146,9 @@ while(n < 2){
       }
     }
 }
+    readsyskey();
     printf("\n\nInit OK \n\n\n");
+
 }
 
 void readsyskey(){
@@ -220,16 +195,14 @@ int main(int argc , char *argv[])
     //cport_nr=strtoul(argv[1],NULL,0);/* /dev/ttyS0 (COM1 on windows) */
     bdrate=9600;       /* 9600 baud */
 
-
-
-
     char mode[]={'8','E','2',0};
     uint8_t tempkey[16],cmac[16];
-    unsigned char updatebuffer[1024] = {0};
+
     SHA1_CTX sha;
     unsigned char *buf;
     int n;
     uint8_t index;
+    char cmd[5] = "\xdd\xF1\x00\x00\x03";
 
     for(int i = 1;i<argc;i++){
         if(!strcmp(argv[i],"--port")){
@@ -269,17 +242,19 @@ uint8_t emmbuffer[0x50] ={
   }
     init();
 
-    command("\xdd\x26\x00\x00\x03","\x69\x01\x00",100);
-    command("\xdd\x26\x00\x00\x03","\x10\x01\x40",100);
-    command("\xdd\x26\x00\x00\x03","\x1c\x01\x00",100);
-    command("\xdd\x82\x00\x00\x14","\x11\x12\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x09\x04\x0B\x00\xFF\xFF\xFF\xFF\xFF\xFF",100);
+
+    command("\xdd\x82\x00\x00\x14","\x11\x12\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x09\x04\x0B\x00\xFF\xFF\xFF\xFF\xFF\xFF",500);
+    command("\xdd\x26\x00\x00\x03","\x69\x01\x00",500);
+    command("\xdd\x26\x00\x00\x03","\x10\x01\x40",500);
+    command("\xdd\x26\x00\x00\x03","\x1c\x01\x00",500);
+
 
     printf("NEW DATA FOR THIS CARD:\n##############################################\n");
     printf("NEW PPUA: %d\t%08X\n",serialno,serialno);
     printf("NEW PPSA: %d\t%08X\n",groupid,groupid);
     printf("NEW TRANSPORT SECRET:\t%s\n",argv[index]);
 
-    readsyskey();
+
     printf("\n\n!!! IF YOU PROCEED, YOU WILL ERASE YOUR CARD !!!\n");
     printf("\n\n!!! PRESS ENTER FOR PROCEED OR CTRL+C FOR ABORT !!!\n");
     getchar();
@@ -295,11 +270,11 @@ uint8_t emmbuffer[0x50] ={
     // SEND PERSOKEY TO CARD
     strcat(&updatebuffer[0],argv[index]);
     strcat(&updatebuffer,(char*)&cmac);
-    unsigned char cmd[5] = "\xdd\xF1\x00\x00\x03";
+
     cmd[4] = strlen(argv[index])+16;
 
 
-    command(cmd,updatebuffer,1000);
+    command(cmd,updatebuffer,500);
 
     memset(emm.in,0xcc,0x30);
     int idx = 0;
@@ -326,6 +301,7 @@ uint8_t emmbuffer[0x50] ={
     memcpy(&emmbuffer[0x40],&cmac,16);
 
     command("\xDD\x84\x00\x00\x50",emmbuffer,1000);
+    //command("\xdd\x26\x00\x00\x03","\x1c\x01\x00",500);
 
     return(0);
 }
